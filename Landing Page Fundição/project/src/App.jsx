ff1d304eb70a86c3ae3cb8f8a8b0c2ff5942554f
Gabriel Fiorim Esteves <123314503+Fiorim1@users.noreply.github.com>
2025-05-22 14:14:19 -0300

    Update App.jsx

--- Landing Page Fundição/project/src/App.jsx
+++ Landing Page Fundição/project/src/App.jsx
@@ -5,6 +5,8 @@ import Bodytwo from './components/Bodytwo'
 import History from './components/History'
 import Bodythree from './components/bodythree'
 import Bodyfor from './components/Bodyfor'
+import Cot from './components/Cot'
+import Footer from './components/Footer'
 
 
 function App() {
@@ -19,6 +21,8 @@ function App() {
         <History />
         <Bodythree />
         <Bodyfor />
+        <Cot />
+        <Footer />
       </div>
     </>
   )
